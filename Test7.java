1a66aeec8f821b0603aaa538609c579dfc734c31
Onur Islak <javasolutionsonur@gmail.com>
2021-09-05 19:45:43 +0300

    Update Test7.java

--- Test7.java
+++ Test7.java
@@ -7,81 +7,131 @@ public class Test7 {
     public static void main(String[] args)
     {
 
-       Scanner scanner = new Scanner(System.in);
-       StandardDeck deck = new StandardDeck();
-       deck.shuffle();
-       List<Card> standardDeck = deck.getCards();
-       LinkedList<Card> cards = (LinkedList<Card>) standardDeck;
-       LinkedList<Card> middle = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
+        StandardDeck deck = new StandardDeck();
+        deck.shuffle();
+        List<Card> standardDeck = deck.getCards();
+        LinkedList<Card> cards = (LinkedList<Card>) standardDeck;
 
-       for(int i=1;i<=4;i++)
-       {
-           if(i==4)
-               middle.push(cards.remove());
-           else
-               middle.add(cards.remove());
-       }
+        LinkedList<Card> middle = new LinkedList<>();
 
-       List<Card>  takenByMe = new LinkedList<>();
-       List<Card>  takenByOpponent = new LinkedList<>();
-       do{
-           LinkedList<Card> myCards = new LinkedList<>();
-           LinkedList<Card> opponentCards = new LinkedList<>();
-          for(int i=1;i<=4;i++)
-              myCards.add(cards.remove());
+        for(int i=1;i<=4;i++)
+        {
+            if(i==4)
+                middle.push(cards.remove());
+            else
+                middle.add(cards.remove());
+        }
 
-          for(int i=1;i<=4;i++)
-              opponentCards.add(cards.remove());
-         if(!middle.isEmpty())
-         Card card = middle.getFirst();
-         System.err.println("card: "+card);
-         System.out.println("middle: "+middle);
-         int count =0;
-         while(count< myCards.size()) {
-             System.out.println("opponent cards: "+opponentCards);
-             //System.out.print("enter 1,2,3,4 from list: " + myCards+" ");
-             print(myCards);
-             int selection = scanner.nextInt();
-             Card selectedCard = myCards.remove(selection - 1);
-             System.out.println("card: "+card);
-             if(middle.isEmpty()&&selectedCard.getRank().value() == card.getRank().value())
-             {
-                 middle.push(selectedCard);
-             }else if (selectedCard.getRank().value() == card.getRank().value()) {
-                 middle.push(selectedCard);
-                 takenByMe.addAll(0,middle);
-                 middle.clear();
+        List<Card>  takenByMe = new LinkedList<>();
+        List<Card>  takenByOpponent = new LinkedList<>();
+        Card card = null;
+        ArrayList<Integer> myP = new ArrayList<>();
+        ArrayList<Integer> opP = new ArrayList<>();
+        do{
+            LinkedList<Card> myCards = new LinkedList<>();
+            LinkedList<Card> opponentCards = new LinkedList<>();
+            for(int i=1;i<=4;i++)
+                myCards.add(cards.remove());
 
-             }else {
-                 middle.push(selectedCard);
-             }
-             System.out.println("opponent searching...");
-             card = search(middle,takenByMe,takenByOpponent,opponentCards);
-             opponentCards.remove(card);
-             if(middle.isEmpty()&&card.getRank()==selectedCard.getRank())
-             {
-                 middle.push(card);
-             }
-             else if(card.getRank()==selectedCard.getRank())
-             {
-                 middle.push(card);
-                 takenByOpponent.addAll(0,middle);
-                 middle.clear();
+            for(int i=1;i<=4;i++)
+                opponentCards.add(cards.remove());
+            if(!middle.isEmpty())
+            card = middle.getFirst();
 
-             }else{
-                 middle.push(card);
-             }
+            System.err.println("card: "+card);
+            System.out.println("middle: "+middle);
+            int count =0;
+            while(count< myCards.size()) {
+                System.out.println("opponent cards: "+opponentCards);
+                System.out.print("enter 1,2,3,4 from list: " + myCards+" ");
+                int selection = scanner.nextInt();
+                Card selectedCard = myCards.remove(selection - 1);
+                if(middle.size()==1&&selectedCard.getRank()==card.getRank())
+                {
+                    System.out.println("pisti....");
+                    middle.push(selectedCard);
+                    takenByMe.addAll(0,middle);
+                    middle.clear();
+                    myP.add(1);
+                }
+                else if(selectedCard.getRank()==Card.Rank.JACK&&middle.isEmpty())
+                {
+                    middle.push(selectedCard);
+                }
+                else if(selectedCard.getRank()==Card.Rank.JACK)
+                {
+                    middle.push(selectedCard);
+                    takenByMe.addAll(0,middle);
+                    middle.clear();
+                }
+                else if(middle.isEmpty()&&selectedCard.getRank().value() == card.getRank().value())
+                {
+                    middle.push(selectedCard);
+                }else if (selectedCard.getRank().value() == card.getRank().value()) {
+                    middle.push(selectedCard);
+                    takenByMe.addAll(0,middle);
+                    middle.clear();
 
-             System.out.println("card: "+card);
-             System.out.println("middle: "+middle);
-             System.out.println("taken by me: "+takenByMe);
-             System.out.println("taken by opponent: "+takenByOpponent);
-         }
-          myCards.clear();
-          opponentCards.clear();
+                }else {
+                    middle.push(selectedCard);
+                }
+                System.out.println("opponent searching...");
+                card = search(middle,takenByMe,takenByOpponent,opponentCards);
+                opponentCards.remove(card);
+                if(middle.size()==1&&selectedCard.getRank()==card.getRank())
+                {
+                    System.out.println("pisti....");
+                    middle.push(card);
+                    takenByOpponent.addAll(0,middle);
+                    middle.clear();
+                    opP.add(1);
+                }
+                else if(card.getRank()==Card.Rank.JACK && middle.isEmpty())
+                {
+                    middle.push(card);
+                }
+                else if(card.getRank()==Card.Rank.JACK)
+                {
+                    middle.push(card);
+                    takenByOpponent.addAll(0,middle);
+                    middle.clear();
+                }
+                else if(middle.isEmpty()&&card.getRank()==selectedCard.getRank())
+                {
+                    middle.push(card);
+                }
+                else if(card.getRank()==selectedCard.getRank())
+                {
+                    middle.push(card);
+                    takenByOpponent.addAll(0,middle);
+                    middle.clear();
 
-       }while(!cards.isEmpty());
-       scanner.close();
+                }else{
+                    middle.push(card);
+                }
+
+
+                System.out.println("middle: "+middle);
+                System.out.println("taken by me: "+takenByMe);
+                System.out.println("taken by opponent: "+takenByOpponent);
+            }
+            myCards.clear();
+            opponentCards.clear();
+
+        }while(!cards.isEmpty());
+        if(middle.size()%2==1)
+        {
+            takenByMe.addAll(0,middle);
+            middle.clear();
+        }else{
+            takenByOpponent.addAll(0,middle);
+            middle.clear();
+        }
+
+        System.out.println("i got this point: "+totalPoints(takenByMe,myP));
+        System.out.println("opponent got this point: "+totalPoints(takenByOpponent,opP));
+        scanner.close();
 
     }
     private static Card search(LinkedList<Card> middle,List<Card> takenByMe,
